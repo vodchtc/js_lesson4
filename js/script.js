4d46eb212139f2e5f43c8127220df2de7af4fcd1
vodchtc <101911163+vodchtc@users.noreply.github.com>
2022-05-23 05:13:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,115 @@
+//1)
+let student = {
+    name: "Darya",
+    surname: "Vodchits",
+    age: 22,
+    course: "frontend",
+    englishLvl: "intermediate",
+};
+
+delete student["englishLvl"];
+
+console.log(student);
+
+//2)
+let book = {
+    numberOfPages: 545,
+    cover: true,
+    isTheAuthorAlive: false,
+    isBestseller: true,
+    booksEditions: 20000000,
+}
+for (let key in book) {
+    if (typeof book[key] === "boolean") {
+        console.log(key)
+    }
+}
+
+//3)
+let car = {
+    brand: "bmw",
+    color: "black",
+    numberOfDoors: 4,
+    horsepower: 265,
+    engineСapacity: 3.0,
+    typeOfGearbox: "ZF8hp",
+};
+let clone = {};
+for (let key in car) {
+    clone[key] = car[key];
+}
+
+
+Object.assign(clone, car);
+
+console.log(car);
+
+//4)
+let animalObj = {
+    name: "cat",
+    legs: 4,
+    color: "black",
+}
+
+function showMe(obj) {
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs`
+}
+
+console.log(showMe({
+    color: 'black',
+    name: 'cat',
+    legs: 4,
+}));
+
+
+//5) 
+let train = {
+    locomotiveLength: 25,
+    totalCoachCars: 4,
+    coachLength: 15,
+}
+
+function calcNumbersOfTrain(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        if (typeof obj[key] === "number") {
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+
+let result = calcNumbersOfTrain(train);
+console.log(result);
+
+//6)
+let studentObj = {
+    name: "Ivan",
+    surname: "Ivanov",
+    numberGroupe: 126,
+}
+
+function showInf(obj) {
+    return `Это студент ${obj.name} ${obj.surname} из группы ${obj.numberGroupe} `
+}
+console.log(showInf({
+    name: 'Ivan',
+    surname: 'Ivanov',
+    numberGroupe: 126,
+}));
+
+//7)
+function createAnimal(animal, legs, isPredator, name) {
+    return {
+        animal: 'true',
+        legs: 4,
+        isPredator: 'true',
+        name: 'lion',
+    }
+}
+console.log(createAnimal({
+    animal: 'true',
+    legs: 4,
+    isPredator: 'true',
+    name: 'lion',
+}));
